ea17a72513fddd7911eddff346c33256ff725788
Anoop Kodadhala <73144205+AnoopKodadhala@users.noreply.github.com>
2022-08-14 23:30:03 -0400

    Added the ability to change attached images

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 import asyncio
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw, ImageEnhance, ImageOps
+import os
 
 client = discord.Client()
 
@@ -12,25 +13,7 @@ client = commands.Bot(command_prefix='!')
 async def on_ready():
     print('Image Manipulator Discord Bot is now running'.format(client))
 
-
-
-
 @client.command()
-async def darker(ctx, user: discord.Member = None):
-    if user == None:
-        user = ctx.author
-    darker = Image.open('black-transparent.png')
-
-    asset = user.avatar_url_as(size=128)
-    data = BytesIO(await asset.read())
-    pfp = Image.open(data)
-
-    '''pfp = pfp.resize((177.177))'''
-    darker.paste(pfp, (120, 212))
-    darker.save("profile.png")
-
-    await ctx.send(file = discord.File('profile.png'))
-
 async def pfp(ctx, user: discord.Member = None ):
     if user == None:
         user = ctx.author
@@ -38,9 +21,102 @@ async def pfp(ctx, user: discord.Member = None ):
     asset = user.avatar_url_as(size=128)
     data = BytesIO(await asset.read())
     pfp = Image.open(data)
-    pfp = pfp.resize((177.177))
-    pfp.save("profile.png")
+    pfp = pfp.resize((177, 177))
+    pfp.save("export.png")
 
-    await ctx.send(file=discord.File(pfp))
+    await ctx.send(file=discord.File('export.png'))
+
+@client.command()
+async def darker(ctx, user: discord.Member = None):
+    files = []
+    attachments = ctx.message.attachments
+    if attachments == files:
+        if user == None:
+            user = ctx.author
+        asset = user.avatar_url_as(size=128)
+        data = BytesIO(await asset.read())
+        pfp = Image.open(data)
+
+        pfp = pfp.resize((177,177))
+        pfp.save("export.png")
+        enhancer = ImageEnhance.Brightness(pfp)
+        im_output = enhancer.enhance(0.3)
+        im_output.save('export.png')
+
+        await ctx.send(file = discord.File('export.png'))
+    else:
+        for attachment in ctx.message.attachments:
+            await attachment.save('export.png')
+        userImage = Image.open('export.png')
+        enhancer = ImageEnhance.Brightness(userImage)
+        im_output = enhancer.enhance(0.3)
+        im_output.save('export.png')
+        await ctx.send(file=discord.File('export.png'))
+
+@client.command()
+async def brighter(ctx, user: discord.Member = None):
+    files = []
+    attachments = ctx.message.attachments
+    if attachments == files:
+        if user == None:
+            user = ctx.author
+        asset = user.avatar_url_as(size=128)
+        data = BytesIO(await asset.read())
+        pfp = Image.open(data)
+        pfp = pfp.resize((177,177))
+        pfp.save("export.png")
+        enhancer = ImageEnhance.Brightness(pfp)
+        im_output = enhancer.enhance(1.7)
+        im_output.save('export.png')
+
+        await ctx.send(file = discord.File('export.png'))
+    else:
+        for attachment in ctx.message.attachments:
+            await attachment.save('export.png')
+        userImage = Image.open('export.png')
+        enhancer = ImageEnhance.Brightness(userImage)
+        im_output = enhancer.enhance(1.7)
+        im_output.save('export.png')
+        await ctx.send(file=discord.File('export.png'))
+
+@client.command()
+async def grayscale(ctx, user: discord.Member = None):
+    files  = []
+    attachments = ctx.message.attachments
+    if attachments == files:
+        if user == None:
+            user = ctx.author
+        asset = user.avatar_url_as(size=128)
+        data = BytesIO(await asset.read())
+        pfp = Image.open(data)
+        pfp = pfp.resize((177,177))
+        pfp = ImageOps.grayscale(pfp)
+        pfp.save('export.png')
+        await ctx.send(file=discord.File('export.png'))
+    else:
+        for attachment in ctx.message.attachments:
+            await attachment.save('export.png')
+        userImage = Image.open('export.png')
+        userImage = ImageOps.grayscale(userImage)
+        userImage.save('export.png')
+
+        await ctx.send(file=discord.File('export.png'))
+
+
+
+
+@client.command()
+async def send_file(ctx, *,user_msg: str):
+    attachments = ctx.message.attachments
+    files = []
+    for attachment in ctx.message.attachments:
+        await attachment.save('export.png')
+    for attachment in attachments:
+        files.append(await attachment.to_file())
+
+    if files:
+        await ctx.send(user_msg, files=files)
+    else:
+        await ctx.send(user_msg)
 
 client.run('MTAwODI1NDM4ODQzMjgwMTgzMg.GNwEjM.WNnQaMK_KwnUYE48Wc5hXxj_6a9vuIAZYYZP10')
